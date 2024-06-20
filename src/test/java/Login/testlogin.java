72b855ba4208bf3c54d901425990408691f4c10b
SAM <147705507+FOGETMENOT@users.noreply.github.com>
2024-06-20 19:26:14 +0530

    Update testlogin.java

--- src/test/java/Login/testlogin.java
+++ src/test/java/Login/testlogin.java
@@ -1,5 +1,12 @@
 package Login;
 
 public class testlogin {
-
+@Test
+public void tc1()
+{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https:\\www.google.com");
+	System.out.println(driver.getTitle());
+	}
 }
