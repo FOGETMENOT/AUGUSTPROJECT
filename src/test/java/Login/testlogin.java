c3b8020663d3d3ac59b5c97a9c46679fa00ad64c
tanvi <sammiksha102@gmail.com>
2024-06-20 19:36:00 +0530

    commit changes

--- src/test/java/Login/testlogin.java
+++ src/test/java/Login/testlogin.java
@@ -1,5 +1,11 @@
 package Login;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class testlogin {
 @Test
 public void tc1()
@@ -9,4 +15,15 @@ public void tc1()
 	driver.get("https:\\www.google.com");
 	System.out.println(driver.getTitle());
 	}
+
+@Test
+public void tc3()
+{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https:\\www.flipkart.com");
+	System.out.println(driver.getTitle());}
+
+
+
 }
